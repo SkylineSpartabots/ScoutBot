fd4910640b270b7f828c4e3aba958c819c5033d1
TwoTau <TwoTau@users.noreply.github.com>
2016-02-17 04:06:23 -0800

    Added more forms and custom input styles
    Radio buttons and checkboxes now have custom styling.
    When a defense <label> is tapped and held, an image of that defense is
    displayed.

--- www/js/index.js
+++ www/js/index.js
@@ -9,18 +9,41 @@ var app = {
             $("#app").attr("class", $(this).attr("id") + "Active");
         });
 
-        var defenseLabels = $("#defensesChosen label, #defensesBreached label:not(.noPicture)");
+        $("label").prepend("<span></span>");
+
+        var defenseLabels = $(".defenses label:not(.noPicture), #autonomous fieldset fieldset legend");
         for(var i = 0; i < defenseLabels.length; i++) {
             var imageUrl = "img/" + $(defenseLabels[i]).text().replace(/ /g,"_").toLowerCase() + ".png";
             var image = $("<img src='" + imageUrl + "'>");
             $(defenseLabels[i]).append(image);
         }
-        defenseLabels.click(function() {
-            $(this).children("img").toggleClass("visible");
+
+        defenseLabels.on("taphold", {
+            duration: 1000
+        }, function() {
+            $(this).children("img").addClass("visible");
+            return false;
+        });
+        defenseLabels.children("img").click(function() {
+            $(this).removeClass("visible");
+            return false;
+        });
+
+        var breachedDefenses = $(".game.view .defenses")[1];
+        var buttonHTML = "<button class='minus'></button><button class='plus'></button>";
+        $(breachedDefenses).children("fieldset").children("span.makes, span.misses").after(buttonHTML);
+        $("span.goals.makes, span.goals.misses").after(buttonHTML);
+        $("button.minus").click(function() {
+            var input = $(this).prevAll("span")[0];
+            if($(input).text() > 0) $(input).text( +$(input).text() - 1);
+        });
+        $("button.plus").click(function() {
+            var input = $(this).prevAll("span")[0];
+            if($(input).text() < 20) $(input).text( +$(input).text() + 1);
         });
 
         $("#generateQR").click(function() {
-            var qrResult = document.getElementById("qrResult");
+            var qrResult = $("#qrResult").empty()[0]; // clear the element
             var qrcode = new QRCode(qrResult, "test data");
         });
 
